9e4e7e938e3a45a180f414bda6367b3d0bf2dc7b
AlexWeidner <64428250+AlexWeidner@users.noreply.github.com>
2020-07-28 12:34:17 +0200

    Add files via upload

--- FridgeScript.py
+++ FridgeScript.py
@@ -163,84 +163,76 @@ def createrecipe(code):
     recipe_file.write(clone)                
     clone_file.close()
     recipe_file.close()
-        # solange Zutaten weniger als 10
-        if i <= 10 :
-            try:
-                ingredientname = str(input('Name der Zutat? ')                                 
-            # wenn Zutatenabfrage mit Enter abbrechen
-            except ingredientname == '' :
-                # Solange Zutaten weniger als 10
-                while i <= 10 :
-                    recipe_path = '/home/pi/Desktop//recipe.txt'
-                    recipe = recipe_file.read()
-                    recipe_file.close()                
-                    clone_path = '/home/pi/Desktop//clone.txt'
-                    clone_file = open(clone_path,'w')                
-                    clone_file.write(recipe)
-                    clone_file.write('\n')                
-                    #leere Zeile
-                    clone_file.write('\n')
-                    #leere Zeile
-                    clone_file.write('\n')
-                    #leere Zeile
-                    clone_file.write('\n')                             
-                    clone_file.close()                
-                    clone_file = open(clone_path,'r')
-                    clone = clone_file.read()
-                    clone_file.close()                
-                    recipe_file = open(recipe_path,'w')
-                    recipe_file.write(clone)                
-                    clone_file.close()
-                    recipe_file.close()
-                    print('Platzhalter ',i,'in Rezept aufgenommen')
-                    i = i + 1
+    # solange Zutaten weniger als 10
+    if i <= 10 :
+        try:
+            ingredientname = str(input('Name der Zutat? '))                                 
+        # wenn Zutatenabfrage mit Enter abbrechen
+        except ingredientname == '' :
+            # Solange Zutaten weniger als 10
+            while i <= 10 :
+                recipe_path = '/home/pi/Desktop//recipe.txt'
+                recipe = recipe_file.read()
+                recipe_file.close()                
+                clone_path = '/home/pi/Desktop//clone.txt'
+                clone_file = open(clone_path,'w')                
+                clone_file.write(recipe)
+                clone_file.write('\n')                
+                #leere Zeile
+                clone_file.write('\n')
+                #leere Zeile
+                clone_file.write('\n')
+                #leere Zeile
+                clone_file.write('\n')                             
+                clone_file.close()                
+                clone_file = open(clone_path,'r')
+                clone = clone_file.read()
+                clone_file.close()                
+                recipe_file = open(recipe_path,'w')
+                recipe_file.write(clone)                
+                clone_file.close()
+                recipe_file.close()
+                print('Platzhalter ',i,'in Rezept aufgenommen')
+                i = i + 1
                                  
-            # wenn Zutatenname nicht abgebrochen
-            else:        
+        # wenn Zutatenname nicht abgebrochen
+        else:        
                     
-                try:
-                    ingredientquan = float(input('Menge der Zutat? '))
+            try:
+                ingredientquan = float(input('Menge der Zutat? '))
                                            
-                except ValueError:
-                    print('Eingabe ist keine Zahl')                       
-                else:
-                    ingredientquan = str(ingredientquan_raw)
-                    ingredientunit = str(input('Einheit der Zutat? '))
-                    recipe_path = '/home/pi/Desktop//recipe.txt'
-                    recipe = recipe_file.read()
-                    recipe_file.close()                
-                    clone_path = '/home/pi/Desktop//clone.txt'
-                    clone_file = open(clone_path,'w')                
-                    clone_file.write(recipe)
-                    clone_file.write('\n')                
-                    clone_file.write(ingredientname)
-                    clone_file.write('\n')
-                    clone_file.write(ingredientquan)
-                    clone_file.write('\n')
-                    clone_file.write(ingredientunit)
-                    clone_file.write('\n')                             
-                    clone_file.close()                
-                    clone_file = open(clone_path,'r')
-                    clone = clone_file.read()
-                    clone_file.close()                
-                    recipe_file = open(recipe_path,'w')
-                    recipe_file.write(clone)                
-                    clone_file.close()
-                    recipe_file.close()
-                    print(ingredientquan,ingredientunit,' ',ingredientname,' als Zutat aufgenommen')
-                    i = i + 1
+            except ValueError:
+                print('Eingabe ist keine Zahl')                       
+            else:
+                ingredientquan = str(ingredientquan_raw)
+                ingredientunit = str(input('Einheit der Zutat? '))
+                recipe_path = '/home/pi/Desktop//recipe.txt'
+                recipe = recipe_file.read()
+                recipe_file.close()                
+                clone_path = '/home/pi/Desktop//clone.txt'
+                clone_file = open(clone_path,'w')                
+                clone_file.write(recipe)
+                clone_file.write('\n')                
+                clone_file.write(ingredientname)
+                clone_file.write('\n')
+                clone_file.write(ingredientquan)
+                clone_file.write('\n')
+                clone_file.write(ingredientunit)
+                clone_file.write('\n')                             
+                clone_file.close()                
+                clone_file = open(clone_path,'r')
+                clone = clone_file.read()
+                clone_file.close()                
+                recipe_file = open(recipe_path,'w')
+                recipe_file.write(clone)                
+                clone_file.close()
+                recipe_file.close()
+                print(ingredientquan,ingredientunit,' ',ingredientname,' als Zutat aufgenommen')
+                i = i + 1
                 
-        else:
-            print('Zutatenliste komplett')
-            i = 1
-        
-        
-def modifyrecipe(code):
-    #blank
-def compareitems(code):
-    
-def updatecookables(code):
-
+    else:
+        print('Zutatenliste komplett')
+        i = 1
 
 # erstelle eine Funktion mit dem Namen main die keine Argumente erwartet
 def main():
@@ -327,15 +319,6 @@ def main():
                         
                         print("Rezept bereits vorhanden")
                         mode = 0
-                
-            elif mode == 4 :
-                #platzhalter
-                
-            elif mode == 5 :
-                #compare items
-                
-            elif mode == 6 :
-                #update cookables
             
             elif mode == 0 :
             
